192e1cbf6d44383d077cda5474564be295d48668
Nishant <nishant52152@gmail.com>
2026-06-12 10:17:59 +0530

    Day-12 uploaded

---
+++ Day-12/Q48.C
@@ -0,0 +1,29 @@
+//Write a Function to Check Perfect Number
+#include <stdio.h>
+#include <stdbool.h>
+
+bool isPerfect(int num) {
+    int sum = 0;
+    
+    for (int i = 1; i < num; i++) {
+        if (num % i == 0) {
+            sum += i;
+        }
+    }
+    
+    return sum == num;
+}
+
+int main() {
+    int number;
+    printf("Enter a number: ");
+    scanf("%d", &number);
+    
+    if (isPerfect(number)) {
+        printf("%d is a perfect number.\n", number);
+    } else {
+        printf("%d is not a perfect number.\n", number);
+    }
+    
+    return 0;
+}
